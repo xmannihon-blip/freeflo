222e54ac66fbd1af1965f994e1b455b0bb5a5aa7
xmannihon-blip <xmannihon@gmail.com>
2026-02-28 21:06:27 -0800

    Add files via upload

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") return res.sendStatus(204);
+  next();
+});
+
 /* ===============================
    HOME PAGE (Freeflo UI)
 ================================ */
